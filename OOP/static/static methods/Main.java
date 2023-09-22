4f7d140c2e91679b9e07aebe71d24d28e5a9abf7
siddheshsurve <sysurve24@gmail.com>
2023-09-22 09:24:44 +0530

    static method

---
+++ OOP/static/static methods/Main.java
@@ -0,0 +1,20 @@
+
+class A {
+    static void display() {
+        System.out.println("Method from A");
+    }
+}
+
+public class Main {
+    static void show() {
+        System.out.println("Method from main class");
+    }
+
+    public static void main(String[] args) {
+        // Main m = new Main();
+        // m.show();
+        Main.show();
+        // or simply show();
+        A.display();
+    }
+}
